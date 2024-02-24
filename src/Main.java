7a8583f28bd4f0ae99321e5a31a93d4a0fb0ce44
Aleks Ka <aleksandra.bm@mail.ru>
2024-02-24 23:21:08 +0300

    Third commit

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-       // BonusMilesService service = new BonusMilesService();
-       // int price = 12_000;
-       // int miles = service.calculate(price);
-       // System.out.println("Начислено миль за покупку: " + miles);
 
         BonusMilesService service = new BonusMilesService();
 
         System.out.println();
         System.out.println("Начислено миль за покупку: ");
-        System.out.println(service.calculate( 12_000));
+        System.out.println(service.calculate(12_000));
 
         System.out.println();
         System.out.println("Начислено миль за покупку: ");
-        System.out.println(service.calculate( 15_000));
+        System.out.println(service.calculate(15_000));
 
         System.out.println();
         System.out.println("Начислено миль за покупку: ");
-        System.out.println(service.calculate( 10_000));
+        System.out.println(service.calculate(10_000));
     }
 }
